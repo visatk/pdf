582f5b7cd71faed8cb482c74b77916216953ca2e
visatk29 <visatk29@gmail.com>
2026-02-16 14:54:22 +0000

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import path from "path"
 import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
+import { cloudflare } from "@cloudflare/vite-plugin"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), cloudflare(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
